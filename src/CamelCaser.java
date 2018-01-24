2c5e8cb7f6ce48747c1ae4f0a35483d3a6e06cdf
Shivani Jain <jainshivani99@yahoo.com>
2018-01-23 21:03:10 -0600

    Enhancing test cases

--- src/CamelCaser.java
+++ src/CamelCaser.java
@@ -1,5 +1,15 @@
 public class CamelCaser {
     public static String camelCase(String inputString){
+        String characterRegex = "/^[!@#$%^&*()_+\\-=\\[\\]{};':|,.\"<>/?]*$/";
+        if (inputString.equals(null)) {
+            throw new IllegalArgumentException(ErrorConstants.INPUT_NULL);
+        }
+        if (inputString.matches(characterRegex)) {
+            throw new IllegalArgumentException(ErrorConstants.INVALID_CHARACTER);
+        }
+        if (inputString.substring(0,1).matches("[0-9]")) {
+            throw new IllegalArgumentException(ErrorConstants.INVALID_FORMAT);
+        }
         inputString = inputString.toLowerCase();
         String[] words = inputString.split(" ");
         String finalOutput = words[0];
--- tests/CamelCaserTest.java
+++ tests/CamelCaserTest.java
@@ -8,12 +8,72 @@ public class CamelCaserTest {
     public void handoutTestCamelCase() {
         assertEquals("thisIsMyString", CamelCaser.camelCase("this is my string"));
     }
+
     @Test
     public void firstLetterCapital() {
         assertEquals("thisIsMyString", CamelCaser.camelCase("This is my string"));
     }
+
     @Test
     public void upperCaseWord() {
         assertEquals("thisIsMyString", CamelCaser.camelCase("This is MY string"));
     }
+
+    @Test
+    public void authorizedDigit() {
+        assertEquals("myNameIsShivani1", CamelCaser.camelCase("My name is Shivani1"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testCharacter() {
+        CamelCaser.camelCase("My name is @Shivani");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void illegalCharacter() {
+        CamelCaser.camelCase("My name, is Shivani");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void emptyString() {
+        CamelCaser.camelCase("");
+    }
+
+    @Test
+    public void oneWord() {
+        assertEquals("this", CamelCaser.camelCase("This"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void unauthorizedDigit() {
+        CamelCaser.camelCase("5Shivani");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void spaceInput() {
+        CamelCaser.camelCase(" ");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void doubleSpaceInput() {
+        CamelCaser.camelCase("  ");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void startWithSpace() {
+        CamelCaser.camelCase(" Shivani");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void onlyIllegalCharacter() {
+        CamelCaser.camelCase("&");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void nullInput() {
+        CamelCaser.camelCase(null);
+    }
+
+
+
 }
